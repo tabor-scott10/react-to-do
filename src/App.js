bcfd90dc89cbd6f9464bfcb7c5672f83025583eb
Scott <tabor.scott10@gmail.com>
2019-02-01 13:57:59 -0500

    Added a state and props to my todo component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Clean the Bathroom', isCompleted: true },
+        { description: 'Walk the Dog', isCompleted: false },
+        { description: 'Wash the Car', isCompleted: false }
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
       <ul>
-        <ToDo />
-        <ToDo />
+        { this.state.todos.map( (todo, index) =>
+        <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} />)}
       </ul>
       </div>
     )
